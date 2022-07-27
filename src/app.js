f20f46dfaf3dde2eaa417bf63eac37c8c0d57b95
wook <wook@neowiz.com>
2022-07-27 19:06:57 +0900

    test

--- src/app.js
+++ src/app.js
@@ -3,18 +3,19 @@
 // querySelector - html 요소를 선택할 때 사용되며 가장 첫번째 요소를 선택
 // id의 경우 #을 붙이고 class인 경우 .를 붙여서 선택, 태그는 태그 그대로
 // Html에서 class 이름이 toDoForm을 찾아서 첫번째 요소를 리턴
-const toDoForm = document.querySelector('.toDoForm');
-const toDoInput = toDoForm.querySelector('input');
-const toDos = document.querySelector('.toDos');
-const addTodoButton = document.querySelector('.addBtn');
-const todoItem = document.querySelector('.li');
+const toDoForm = document.querySelector(".toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDos = document.querySelector(".toDos");
+const addTodoButton = document.querySelector(".addBtn");
+const todoItem = document.querySelector(".li");
 
-const TODOLIST = 'toDoList'; // 추가
+const TODOLIST = "toDoList"; // 추가
 let toDoList = []; // 추가
 
 function loadToDoList() {
   const loadedToDoList = localStorage.getItem(TODOLIST);
   if (loadedToDoList !== null) {
+    // JSON.parse - String 객체를 json 객체로 변환
     const parsedToDoList = JSON.parse(loadedToDoList);
     for (const toDo of parsedToDoList) {
       const { text } = toDo; // Const text = toDo;
@@ -28,9 +29,9 @@ function init() {
   loadToDoList(); // 추가
   // https://developer.mozilla.org/ko/docs/Web/API/EventTarget/addEventListener
   // https://developer.mozilla.org/ko/docs/Web/Events
-  toDoForm.addEventListener('submit', createToDo);
-  addTodoButton.addEventListener('click', createToDo);
-  todoItem.addEventListener('click', completeTodo);
+  toDoForm.addEventListener("submit", createToDo);
+  addTodoButton.addEventListener("click", createToDo);
+  todoItem.addEventListener("click", completeTodo);
 }
 
 init();
@@ -41,6 +42,10 @@ function saveToDo(toDo) {
     id: toDoList.length + 1,
   };
   toDoList.push(toDoObject);
+  // 브라우저에 key-value 값을 storage에 저장
+  // setItem(key, value) - 아이템 추가
+  // getItem(key) - 아이템을 읽기 위해 사용
+  // JSON.stringify - json 객체를 String 객체로 변환
   localStorage.setItem(TODOLIST, JSON.stringify(toDoList));
 }
 
@@ -49,23 +54,35 @@ function createToDo(event) {
   const toDo = toDoInput.value;
   paintToDo(toDo);
   saveToDo(toDo);
-  toDoInput.value = '';
+  toDoInput.value = "";
 }
 
 function paintToDo(toDo) {
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  const delButton = document.createElement('span');
-  delButton.textContent = 'X';
-  delButton.classList.add('close');
-  delButton.addEventListener('click', delToDo);
+  // JS에서 html 요소를 생성하기 위해 createElement 사용 (li, span)
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const checkbox = document.createElement("input");
+  const delButton = document.createElement("span");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("checkbox");
+
+  delButton.textContent = "X";
+  delButton.classList.add("close");
+  delButton.addEventListener("click", delToDo);
   span.innerHTML = toDo;
+  li.append(checkbox);
   li.append(span);
   li.append(delButton);
   li.id = toDoList.length + 1;
-  li.addEventListener('click', function () {
+  li.addEventListener("click", function () {
     completeTodo(li.id);
   });
+  // checkbox?
+  checkbox.addEventListener("click", function () {
+    // completeTodo(li.id);
+    console.log("checked");
+  });
+
   toDos.append(li);
 }
 
@@ -79,9 +96,66 @@ function delToDo(event) {
 
 function completeTodo(id) {
   const item = document.getElementById(id);
-  if (item.classList.contains('checked')) {
-    item.classList.remove('checked');
+  if (item.classList.contains("checked")) {
+    item.classList.remove("checked");
   } else {
-    item.classList.add('checked');
+    item.classList.add("checked");
+  }
+}
+
+/////////////////////////////////////////
+// remove todo item
+
+function removeTodoItem(elem) {
+  elem.remove();
+  updateItemsCount(-1);
+}
+
+TODOLIST.addEventListener("click", (event) => {
+  if (event.target.classList.contains("remove")) {
+    removeTodoItem(event.target.parentElement);
+  }
+});
+
+// clear comleted items
+
+document.querySelector(".clear").addEventListener("click", () => {
+  document
+    .querySelectorAll('.list-item input[type="checkbox"]:checked')
+    .forEach((item) => {
+      removeTodoItem(item.closest("li"));
+    });
+});
+
+// filter todo list items
+document.querySelectorAll(".filter input").forEach((radio) => {
+  radio.addEventListener("change", (e) => {
+    filterTodoItems(e.target.id);
+  });
+});
+
+function filterTodoItems(id) {
+  const allItems = TODOLIST.querySelectorAll("li");
+
+  switch (id) {
+    case "all":
+      allItems.forEach((item) => {
+        item.classList.remove("hidden");
+      });
+      break;
+    case "active":
+      allItems.forEach((item) => {
+        item.querySelector("input").checked
+          ? item.classList.add("hidden")
+          : item.classList.remove("hidden");
+      });
+      break;
+    default:
+      allItems.forEach((item) => {
+        !item.querySelector("input").checked
+          ? item.classList.add("hidden")
+          : item.classList.remove("hidden");
+      });
+      break;
   }
 }
